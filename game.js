d3c588ff9ce2caccf322b5a5a339d97c2ea8291e
karim <haddadi.karim20@gmail.com>
2023-11-10 15:34:43 +0100

    correction de bug

--- game.js
+++ game.js
@@ -21,8 +21,8 @@ background4.src = "img/backgroundnew4.jpg";
 let backgroundX = 0;
 let backgrounds = [background1, background2, background3, background4];
 
-// let deathSound = new Audio();
-// deathSound.src = "sounds/zemm.mp3";
+let deathSound = new Audio();
+deathSound.src = "sounds/zemm.mp3";
 
 let player1Width = 90;
 let player1Height = 80;
@@ -36,8 +36,8 @@ let player1 = {
   y: player1Y,
   width: player1Width,
   height: player1Height,
-  velocity: 0,
-  gravity: 0.5
+  velocity: player1Velocity,
+  gravity: player1Gravity,
 };
 
 let toppipe = new Image();
@@ -49,6 +49,62 @@ bottompipe.src = "img/bottompipe.png";
 let musicSound = new Audio();
 musicSound.src = "sounds/music.mp3";
 musicSound.loop = true;
+
+let musicAntonin = new Audio();
+musicAntonin.src = "sounds/music.mp3";
+musicAntonin.loop = true;
+
+let musicKarim = new Audio();
+musicKarim.src = "sounds/karim.mp3";
+musicKarim.loop = true;
+
+let globalData;
+
+let toppipeWidth;
+let bottompipeWidth;
+let pipeOscillationSpeedON;
+let backgroundurl;
+
+function getNames() {
+  return new Promise((resolve, reject) => {
+    fetch('getName.php')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        globalData = data;
+        toppipeWidth = data[0].width;
+        bottompipeWidth = data[0].width;
+        pipeOscillationSpeedON = data[0].hSpeed;
+        backgroundurl = data[0].background;
+        resolve(data);
+        console.log(toppipeWidth)
+      })
+      .catch((error) => {
+        console.log('There has been a problem:', error);
+        reject(error);
+      });
+  });
+}
+
+getNames()
+  .then(() => {
+    drawPipes();
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+
+document.onkeydown = function (event) {
+  if (event.code === "Space") {
+    event.preventDefault();
+  }
+};
+
 const button = document.querySelector("#onoff");
 
 const karim = document.querySelector("#karim");
@@ -67,12 +123,14 @@ button.addEventListener("click", function () {
     button.style.backgroundColor = "rgb(244, 64, 64)";
   }
 });
+if (karim != undefined) {
+  karim.addEventListener("click", function () {
+    karim.blur();
+    musicSound.src = musicKarim.src;
+    musicSound.play();
+  });
+}
 
-karim.addEventListener("click", function () {
-  karim.blur();
-  musicSound.src = musicKarim.src;
-  musicSound.play();
-});
 
 antonin.addEventListener("click", function () {
   antonin.blur();
@@ -114,7 +172,7 @@ function randomHeight(min, max) {
 
 // let toppipeWidth = 40;
 let toppipeHeight = randomHeight(100, 230);
-let toppipeSpeed = 5;
+let toppipeSpeed = 0;
 let toppipeX = boardWidth;
 let toppipeY = 0;
 
@@ -129,21 +187,125 @@ let pipeOscillationSpeed = 0;
 
 let pipes = [];
 
+let gameIsRunning = false;
+
+function startGame() {
+
+  const message = document.createElement("div");
+  message.textContent = "Appuyer sur espace pour jouer";
+
+  message.style.textAlign = "center";
+  message.style.verticalAlign = "middle";
+
+  message.style.position = "absolute";
+  message.style.left = "50%";
+  message.style.top = "50%";
+  message.style.color = "white";
+  message.style.fontSize = "40px";
+  message.style.transform = "translate(-50%, -50%)";
+
+  document.body.appendChild(message);
+
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Space") {
+      gameIsRunning = true;
+      message.remove();
+    }
+  });
+}
+startGame();
+
+let gameisOver = false;
+
+function gameOver() {
+  const msgGameOver = document.createElement("div");
+  msgGameOver.textContent = "Game Over ! Score: " + score;
+  msgGameOver.style.position = "absolute";
+  msgGameOver.style.left = "50%";
+  msgGameOver.style.top = "50%";
+  msgGameOver.style.color = "white";
+  msgGameOver.style.fontSize = "40px";
+  msgGameOver.style.transform = "translate(-50%, -50%)";
+
+  const msgBestScore = document.createElement("div");
+  msgBestScore.textContent = "Meilleur score: " + bestScore;
+  msgBestScore.style.position = "absolute";
+  msgBestScore.style.left = "50%";
+  msgBestScore.style.top = "60%";
+  msgBestScore.style.color = "white";
+  msgBestScore.style.fontSize = "40px";
+  msgBestScore.style.transform = "translate(-50%, -50%)";
+
+
+  if (score > bestScore) {
+    bestScore = score;
+    localStorage.setItem('bestScore', bestScore);
+  }
+
+  document.body.appendChild(msgGameOver);
+  document.body.appendChild(msgBestScore);
+  deathSound.play();
+
+  gameisOver = true;
+
+  setTimeout(function () {
+    msgGameOver.remove();
+    msgBestScore.remove();
+  }
+    , 1100);
+
+  // Ajout d'un flou sur l'arrière-plan
+  canvas.style.filter = "blur(5px)";
+
+  toppipeSpeed = 0;
+  bottompipeSpeed = 0;
+  gameIsRunning = false;
+  player1.velocity = 0;
+
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Space" && gameisOver) {
+      msgGameOver.remove();
+      msgBestScore.remove();
+      player1.y = player1Y;
+      toppipeSpeed = 0;
+      bottompipeSpeed = 0;
+      gameIsRunning = true;
+      gameisOver = false;
+      pipes = [];
+      initPipes();
+      score = 0;
+      canvas.style.filter = "none";
+    }
+  });
+
+}
+
+function resetGame() {
+  player1.y = player1Y;
+  player1.velocity = 0;
+  player1.gravity = 0.5;
+  toppipeSpeed = 0;
+  bottompipeSpeed = 0;
+  gameIsRunning = false;
+  startGame();
+
+  pipes = [];
+
+  initPipes();
+
+  score = 0;
+}
+
+
 function initPipes() {
   pipes.push({
     toppipeX: boardWidth,
     toppipeY: 0,
-    toppipeHeight: randomHeight(100, 230),
+    toppipeHeight: randomHeight(150, 290),
     bottompipeX: boardWidth,
     bottompipeY: bottompipeY,
     bottompipeHeight: randomHeight(-150, -240),
     passed: false
-
-    toppipeHeight: randomHeight(100, 330),
-    bottompipeX: boardWidth,
-    bottompipeY: bottompipeY,
-    bottompipeHeight: bottompipeHeight,
-    passed: false, // Nouvelle propriété pour marquer le tuyau comme non passé
   });
 
   pipes.push({
@@ -152,7 +314,7 @@ function initPipes() {
     toppipeHeight: randomHeight(150, 290),
     bottompipeX: boardWidth + 300,
     bottompipeY: bottompipeY,
-    bottompipeHeight: randomHeight(-150, -270),
+    bottompipeHeight: randomHeight(-150, -240),
     passed: false
   });
 }
@@ -178,83 +340,55 @@ function drawPipes() {
     if (pipes[i].toppipeX + toppipeWidth < player1X && !pipes[i].passed) {
       score += 1;
       pipes[i].passed = true;
-      // Le joueur a passé ce tuyau
-      score += 1; // Incrémente le score de 1 à chaque passage de tuyau
-      pipes[i].passed = true; // Marque le tuyau comme passé
     }
 
     if (pipes[i].toppipeX + toppipeWidth < 0) {
       pipes[i].toppipeX = boardWidth;
-      pipes[i].toppipeHeight = randomHeight(100, 230);
+      pipes[i].toppipeHeight = randomHeight(100, 260);
       pipes[i].passed = false;
     }
 
     if (pipes[i].bottompipeX + bottompipeWidth < 0) {
       pipes[i].bottompipeX = boardWidth;
-      pipes[i].bottompipeHeight = randomHeight(-150, -270);
+      pipes[i].bottompipeHeight = randomHeight(-150, -240);
       pipes[i].passed = false;
-
-      pipes[i].bottompipeHeight = bottompipeHeight
     }
   }
 }
 
-function drawPlayer1(player) {
-  context.drawImage(player1Img, player.x, player.y, player.width, player.height);
-}
 
-function checkPlayerPipeCollisions(player, pipes) {
-  for (let i = 0; i < pipes.length; i++) {
-    let pipe = pipes[i];
-
-    // Vérifiez la collision avec le haut du tuyau
-    if (
-      player.x + player.width > pipe.toppipeX &&
-      player.x < pipe.toppipeX + toppipeWidth &&
-      player.y < pipe.toppipeY + pipe.toppipeHeight
-    ) {
-      return true;
-    }
-
-    // Vérifiez la collision avec le bas du tuyau
-    if (
-      player.x + player.width > pipe.bottompipeX &&
-      player.x < pipe.bottompipeX + bottompipeWidth &&
-      player.y + player.height > boardHeight + pipe.bottompipeHeight
-    ) {
-      return true;
-    }
-  }
-  return false;
-}
-
+const canvas = document.getElementById('background');
+const ctx = canvas.getContext('2d');
 
 
 window.onload = function () {
+
+
   board = document.getElementById("background");
   board.height = boardHeight;
   board.width = boardWidth;
   context = board.getContext("2d");
 
+  playerGreenImg = new Image();
+  playerGreenImg.src = "img/playerGreen.png";
 
-  player1Img = new Image();
-  player1Img.src = "img/player1.png";
+  playerBlueImg = new Image();
+  playerBlueImg.src = "img/playerBlue.png";
 
-  player2Img = new Image();
-  player2Img.src = "img/player2.png";
+  playerPurpleImg = new Image();
+  playerPurpleImg.src = "img/playerPurple.png";
 
-  player3Img = new Image();
-  player3Img.src = "img/player3.png";
+  playerRedImg = new Image();
+  playerRedImg.src = "img/playerRed.png";
 
-  player4Img = new Image();
-  player4Img.src = "img/player4.png";
+  playerYellowImg = new Image();
+  playerYellowImg.src = "img/playerYellow.png";
 
-  player1.width = 60;
-  player1.height = 50;
+  player1.width = 50;
+  player1.height = 40;
 
-  let players = [player1Img, player2Img, player3Img, player4Img];
-  let currentPlayerImg = players[Math.floor(Math.random() * players.length)];
 
+  let players = [playerGreenImg, playerBlueImg, playerPurpleImg, playerRedImg, playerYellowImg];
 
   toppipe = new Image();
   toppipe.src = "img/toppipe.png";
@@ -264,20 +398,88 @@ window.onload = function () {
 
   initPipes();
 
+
   let randomBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)];
   context.drawImage(randomBackground, 0, 0, boardWidth, boardHeight);
-  drawPlayer1(player1);
-
-  document.addEventListener("keydown", function (event) {
-    if (event.code === "Space") {
-      player1.velocity = -10;
-    }
-  });
-
   document.getElementById("background").style.backgroundImage = `url('${randomBackground.src}')`;
 
+  const carouselImages = document.querySelectorAll(".carousel img");
 
-  // ... (autres parties de votre code)
+  carouselImages.forEach((image) => {
+    image.addEventListener("click", () => {
+      const newBackground = image.getAttribute("src");
+      randomBackground.src = newBackground;
+    });
+  });
+
+
+  let SelectedPlayerImg = players[Math.floor(Math.random() * players.length)];
+  context.drawImage(SelectedPlayerImg, player1.x, player1.y, player1.width, player1.height);
+
+
+  const carouselPlayerImages = document.querySelectorAll(".players img");
+
+  carouselPlayerImages.forEach((image) => {
+    image.addEventListener("click", () => {
+      console.log("click");
+      const newPlayerImage = image.getAttribute("src");
+      SelectedPlayerImg.src = newPlayerImage;
+    });
+  });
+
+
+
+  drawPlayer1(player1);
+
+  if (gameisOver) {
+    document.addEventListener("keydown", function (event) {
+      if (event.code === "Space") {
+        player1.velocity = 0;
+        player1.gravity = 0;
+      }
+    })
+  } else {
+    document.addEventListener("keydown", function (event) {
+      if (event.code === "Space") {
+        player1.velocity = -10;
+      }
+    })
+  }
+
+  function drawPlayer1(player) {
+    context.drawImage(SelectedPlayerImg, player.x, player.y, player.width, player.height);
+  }
+
+  function checkPlayerPipeCollisions(player, pipes) {
+    for (let i = 0; i < pipes.length; i++) {
+      let pipe = pipes[i];
+
+      // Vérifiez la collision avec le haut du tuyau
+      if (
+        player.x + player.width > pipe.toppipeX &&
+        player.x < pipe.toppipeX + toppipeWidth &&
+        player.y < pipe.toppipeY + pipe.toppipeHeight
+      ) {
+        return true;
+      }
+
+      // Vérifiez la collision avec le bas du tuyau
+      if (
+        player.x + player.width > pipe.bottompipeX &&
+        player.x < pipe.bottompipeX + bottompipeWidth &&
+        player.y + player.height > boardHeight + pipe.bottompipeHeight
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function drawScore() {
+    context.fillStyle = "white";
+    context.font = "25px Mina";
+    context.fillText("Score: " + score, 10, 34);
+  }
 
 
   setInterval(function () {
@@ -323,17 +525,4 @@ window.onload = function () {
 
   }, 1000 / 60);
 
-}
-
-function resetGame() {
-  // deathSound.play();
-  player1.y = player1Y;
-  player1.velocity = 0;
-  player1.gravity = 0.5;
-
-  pipes = [];
-
-  initPipes(); // Réinitialisez les tuyaux ici
-
-  score = 0;
 }
